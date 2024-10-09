fad2a27922b3707d3fad157e25c0f31e8693d3e2
Andrey <andrey.mikhaylov.n@gmail.com>
2024-10-09 13:30:26 +0300

    done

--- module_6_1.py
+++ module_6_1.py
@@ -1,4 +1,76 @@
+class Plant:
+    def __init__(self, name: str):
+        print(name)
+        self.edible = False
+        self.name = name
 
+
+class Animal:
+    def __init__(self, name: str):
+        print(name)
+        self.name = name
+        self.alive = True
+        self.fed = False
+
+    def eat(self, food: Plant):
+        if food.edible:
+            print(f"{self.name} съел {food.name}")
+            self.fed = True
+        else:
+            print(f"{self.name} не стал есть {food.name}")
+            self.alive = False
+
+
+class Mammal(Animal):
+    pass
+
+
+class Predator(Animal):
+    pass
+
+
+class Flower(Plant):
+    pass
+
+
+class Fruit(Plant):
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.edible = True
+
+
+def test():
+    a1 = Predator('Волк с Уолл-Стрит')
+    a2 = Mammal('Хатико')
+    p1 = Flower('Цветик семицветик')
+    p2 = Fruit('Заводной апельсин')
+
+    print(a1.name)
+    print(p1.name)
+
+    print(a1.alive)
+    print(a2.fed)
+    a1.eat(p1)
+    a2.eat(p2)
+    print(a1.alive)
+    print(a2.fed)
+
+    # Что произошло: Хищник попытался съесть цветок и погиб, млекопитающее съело фрукт и насытилось.
+    """
+    Вывод на консоль:
+    Волк с Уолл-Стрит
+    Цветик семицветик
+    True
+    False
+    Волк с Уолл-Стрит не стал есть Цветик семицветик
+    Хатико съел Заводной апельсин
+    False
+    True
+    """
+
+
+if __name__ == "__main__":
+    test()
 
 
 """
